30ee17daadba50c2702a52c279b4e96ac0f9cdc4
Cosmin Horjea <horjeacosmin@yahoo.com>
2019-10-24 22:14:08 +0300

    Facut schimbari la structura de fisiere

--- Tutoriate/Programarea algoritmilor/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-    while s[poz:poz+l].isnumeric:
-        l += 1
-    print(s[poz, p
--- Tutoriate/Programarea algoritmilor/teorie.py
+++
@@ -1,5 +0,0 @@
-s = "Ana are mere"
-print(s[::2])  # printez caracterele din 2 in 2
-c = [c for c in s]
-print(c)  # creez o lista cu caracterele lui s
-quit()  # dau exit la program, un fel de return 0
