af8e374e6c951c178b312dfeb16b91a1431c24e5
Cosmin Horjea <horjeacosmin@yahoo.com>
2020-01-19 00:09:13 +0200

    Lab7, curs si seminar la PA

---
+++ Programarea Algoritmilor/laboratoare/lab7/problema4.py
@@ -0,0 +1,3 @@
+f = open('problema4.in')
+
+n, k = map(int, f.readline().split())
---
+++ Programarea Algoritmilor/laboratoare/lab7/test.py
@@ -0,0 +1,7 @@
+from math import sin
+from random import random
+pi = 0
+for i in range(100):
+    pi += sin(random())
+    print(pi)
+# print(random())
