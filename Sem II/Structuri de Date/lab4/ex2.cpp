0302d9bdbdfed317ea522f1ff2a686272f3b45ca
Horjea Cosmin <horjeacosmin@yahoo.com>
2020-04-07 20:02:30 +0300

    Terminat Tema 4 SD

--- Sem II/Structuri de Date/lab4/ex2.cpp
+++ Sem II/Structuri de Date/lab4/ex2.cpp
@@ -1,4 +1,6 @@
 /*
+	Tema 4 : Arbori Binari de Cutare
+	
 	2. (1p) Folositi un arbore binar de cautare ca sa sortati n siruri de caractere. 
 	Puteti folosi strcmp din string.h pentru comparare lexicografica intre siruri de caractere.
 */
@@ -59,8 +61,30 @@ void SRD(arbore *aux)
 int main()
 {
 	string values[4] = {"a", "c", "d", "b"};
+	int n = 4, i = 0;
 	for (int i = 0; i < 4; i++)
 		insert(values[i]);
-	SRD(radacina);
+	SRD(radacina); // aici doar le afisez in ordine
+	cout << endl;
+	arbore **stack = new arbore *[n];
+	unsigned tos = 0;
+	arbore *aux = radacina;
+	while (tos > 0 || aux != 0) // aici sortez efectiv vectorul cu o parcurgere dar nerecursiva
+	{
+		if (aux)
+		{
+			stack[tos++] = aux;
+			aux = aux->stang;
+		}
+		else
+		{
+			aux = stack[--tos];
+			values[i++] = aux->info;
+			aux = aux->drept;
+		}
+	}
+	delete[] stack;
+	for (int i = 0; i < 4; i++)
+		cout << values[i] << " ";
 	cout << endl;
 }
