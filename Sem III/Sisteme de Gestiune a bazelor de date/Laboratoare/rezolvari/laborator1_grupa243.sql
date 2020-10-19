feb5197594c24b6ae1504a61068d42c5f6c0548e
CosminHorjea <horjeacosmin@gmail.com>
2020-10-19 19:44:51 +0300

    materiale la alg fundamentali si sgbd

---
+++ Sem III/Sisteme de Gestiune a bazelor de date/Laboratoare/rezolvari/laborator1_grupa243.sql
@@ -0,0 +1,82 @@
+--Laborator 1
+--1
+--a falsa
+DESC employees;
+--b adevarat
+--c falsa
+
+--2
+--a falsa
+--b PRIMARY KEY(COL1, COL2) falsa
+--c falsa
+--d adevarata
+
+--3
+--a adevarata
+--b adevarata
+--c falsa
+--d adevarata
+
+--4 d
+
+--5 c
+
+--6 a
+SELECT (SELECT first_name
+        FROM employees
+        WHERE employee_id = 100)
+FROM DUAL;
+
+--7 a
+
+--8 c
+
+--9 c
+
+--10 d
+
+--11
+CREATE TABLE emp_prof
+AS SELECT * FROM employees;
+
+COMMENT ON TABLE emp_prof IS 'Informa?ii despre angajati';
+
+--12
+DESC user_tab_comments;
+
+SELECT *
+FROM user_tab_comments
+WHERE INITCAP(table_name) = INITCAP('emp_prof');
+
+--13
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD.MM.YYYY HH24:mi:ss';
+
+SELECT SYSDATE
+FROM DUAL;
+
+--14
+SELECT EXTRACT(YEAR FROM SYSDATE)
+FROM dual;
+
+--15
+SELECT EXTRACT(MONTH FROM SYSDATE)
+FROM dual;
+
+SELECT EXTRACT(DAY FROM SYSDATE)
+FROM dual;
+
+--16
+SELECT *
+FROM user_tables
+WHERE UPPER(table_name) LIKE UPPER('%PRoF');
+
+--17-22
+SET FEEDBACK OFF;
+SET PAGESIZE 0;
+SPOOL sterg_tabele.sql
+SELECT 'DROP TABLE ' || table_name || ' cascade constraint; '
+FROM user_tables
+WHERE UPPER(table_name) LIKE UPPER('%PRoF');
+SPOOL OFF
+
+--23 TEMA
---
+++ Sem III/Sisteme de Gestiune a bazelor de date/Laboratoare/rezolvari/recapitulare2-lab2.sql
@@ -0,0 +1,146 @@
+--Laborator 2 SGBD
+--1
+SELECT *
+FROM rental;
+
+-- member (member_id)
+--title (title_id)
+--title_copy (copy_id si title_id)
+--rental (book_date, copy_id, member_id, title_id)
+--reservation (res_date, member_id, title_id)
+
+--4
+SELECT ti.title_id, title, copy_id
+FROM title_copy ti JOIN title t ON (ti.title_id = t.title_id)
+WHERE category = (SELECT category
+                  FROM rental r JOIN title t ON (r.title_id = t.title_id)
+                  GROUP BY category
+                  HAVING COUNT(*) = (SELECT MAX(COUNT(*))
+                                     FROM rental r JOIN title t ON (r.title_id = t.title_id)
+                                     GROUP BY category));
+
+--5
+SELECT *
+FROM rental;
+
+SELECT *
+FROM title_copy;
+
+SELECT title_id, COUNT(copy_id) AS "CARTI DISP"
+FROM (SELECT copy_id, title_id
+     FROM title_copy
+     MINUS
+     SELECT copy_id, title_id
+     FROM rental
+     WHERE act_ret_date IS NULL)
+GROUP BY title_id;
+
+--6
+--metoda 1
+SELECT t.title_id, title, copy_id, status, 'RENTED' status_corect
+FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+WHERE (t.title_id, copy_id) IN (select title_id, copy_id
+                                FROM rental
+                                WHERE act_ret_date IS NULL)
+UNION
+SELECT t.title_id, title, copy_id, status, 'AVAILABLE' status_corect
+FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+WHERE (t.title_id, copy_id) IN (SELECT title_id, copy_id
+                                FROM title_copy
+                                MINUS
+                                SELECT title_id, copy_id
+                                FROM rental
+                                WHERE act_ret_date IS NULL);
+--metoda 2 (CASE)
+
+--7
+--a
+SELECT COUNT(*)
+FROM (SELECT t.title_id, title, copy_id, status, 'RENTeD' status_corect
+      FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+      WHERE (t.title_id, copy_id) IN (select title_id, copy_id
+                                      FROM rental
+                                      WHERE act_ret_date IS NULL)
+      UNION
+      SELECT t.title_id, title, copy_id, status, 'AVAIlABLE' status_corect
+      FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+      WHERE (t.title_id, copy_id) IN (SELECT title_id, copy_id
+                                      FROM title_copy
+                                      MINUS
+                                      SELECT title_id, copy_id
+                                      FROM rental
+                                      WHERE act_ret_date IS NULL))
+WHERE status <> UPPER(status_corect);
+
+--b
+UPDATE title_copy tr
+SET status = (SELECT UPPER(status_corect)
+              FROM (SELECT t.title_id, title, copy_id, status, 'RENTeD' status_corect
+                    FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+                    WHERE (t.title_id, copy_id) IN (select title_id, copy_id
+                                                    FROM rental
+                                                    WHERE act_ret_date IS NULL)
+                    UNION
+                    SELECT t.title_id, title, copy_id, status, 'AVAIlABLE' status_corect
+                    FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+                    WHERE (t.title_id, copy_id) IN (SELECT title_id, copy_id
+                                                    FROM title_copy
+                                                    MINUS
+                                                    SELECT title_id, copy_id
+                                                    FROM rental
+                                                    WHERE act_ret_date IS NULL))
+                WHERE tr.title_id = title_id
+                AND tr.copy_id = copy_id)
+WHERE (title_id, copy_id) IN (SELECT title_id, copy_id
+                              FROM (SELECT t.title_id title_id, title, copy_id, status, 'RENTeD' status_corect
+                                    FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+                                    WHERE (t.title_id, copy_id) IN (select title_id, copy_id
+                                                    FROM rental
+                                                    WHERE act_ret_date IS NULL)
+                    UNION
+                    SELECT t.title_id title_id, title, copy_id, status, 'AVAIlABLE' status_corect
+                    FROM title t JOIN title_copy ti ON t.title_id = ti.title_id
+                    WHERE (t.title_id, copy_id) IN (SELECT title_id, copy_id
+                                                    FROM title_copy
+                                                    MINUS
+                                                    SELECT title_id, copy_id
+                                                    FROM rental
+                                                    WHERE act_ret_date IS NULL))
+                                WHERE status <> UPPER(status_corect));
+ROLLBACK;
+
+SELECT *
+FROM title_copy;
+
+--8
+SELECT *
+FROM reservation;
+
+SELECT r.member_id, r.title_id, DECODE(r.res_date, re.book_date, 'Da', 'Nu') imp_ac_zi
+FROM reservation r JOIN rental re ON (r.title_id = re.title_id AND r.member_id = re.member_id);
+
+SELECT r.member_id, r.title_id, CASE 
+                                    WHEN r.res_date <> re.book_date THEN 'Nu'
+                                    ELSE 'Da'
+                                END imp_ac_zi
+FROM reservation r JOIN rental re ON (r.title_id = re.title_id AND r.member_id = re.member_id);
+
+SELECT r.member_id, r.title_id, CASE r.res_date
+                                    WHEN re.book_date THEN 'Da'
+                                    ELSE 'Nu'
+                                END imp_ac_zi
+FROM reservation r JOIN rental re ON (r.title_id = re.title_id AND r.member_id = re.member_id);
+
+--9
+SELECT m.member_id, first_name, last_name, title, COUNT(copy_id)
+FROM member m JOIN rental r ON m.member_id = r.member_id
+              JOIN title t ON t.title_id = r.title_id
+GROUP BY m.member_id, r.title_id, first_name, last_name, title;
+
+SELECT m.member_id, MAX(first_name), MAX(last_name), MAX(title), COUNT(copy_id)
+FROM member m JOIN rental r ON m.member_id = r.member_id
+              JOIN title t ON t.title_id = r.title_id
+GROUP BY m.member_id, r.title_id;
+
+--TEMA 2, 3, 6 (alte rezolvari), 10, 11, 12
+
