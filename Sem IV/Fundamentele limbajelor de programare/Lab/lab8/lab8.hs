b649d3a966cfd40c07e927d42a0be6cfa9671f00
CosminHorjea <horjeacosmin@gmail.com>
2021-04-23 11:11:11 +0300

    final stretch for haskell test

--- Sem IV/Fundamentele limbajelor de programare/Lab/lab8/lab8.hs
+++ Sem IV/Fundamentele limbajelor de programare/Lab/lab8/lab8.hs
@@ -1,160 +1,169 @@
 -- Bibliografie:  Graham Hutton, Programming in Haskell, 2nd edition, Chapter 13
 
-import Data.Char ( isDigit, isSpace )
-import Control.Monad ( MonadPlus(..) )
-import Control.Applicative ( Alternative(many, empty, (<|>)) )
+import Control.Applicative (Alternative (empty, many, (<|>)))
+import Control.Monad (MonadPlus (..))
+import Data.Char (isDigit, isSpace, ord)
 
+newtype Parser a = Parser {apply :: String -> [(a, String)]}
 
-
-
-newtype Parser a =
-    Parser { apply :: String -> [(a, String)] }
-    
-    
-      
 parse :: Parser a -> String -> a
-parse m s = head [ x | (x,t) <- apply m s, t == "" ]
+parse m s = head [x | (x, t) <- apply m s, t == ""]
 
--- parse anychar "a123" = head [ x | (x,t) <- apply anychar "a123". t==""] 
-                    
--- Recunoasterea unui caracter arbitrar                                       
+-- parse anychar "a123" = head [ x | (x,t) <- apply anychar "a123". t==""]
+
+-- Recunoasterea unui caracter arbitrar
 anychar :: Parser Char
 anychar = Parser f
-    where
-    f []     = []
-    f (c:s) = [(c,s)]
-    
+  where
+    f [] = []
+    f (c : s) = [(c, s)]
+
 -- Recunoasterea unui caracter cu o proprietate
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
-    where
-    f []                 = []
-    f (c:s) | p c        = [(c, s)]
-            | otherwise = []
+  where
+    f [] = []
+    f (c : s)
+      | p c = [(c, s)]
+      | otherwise = []
 
 -- Recunoasterea unui anumit caracter
 char :: Char -> Parser Char
-char c = satisfy (== c)     
-
+char c = satisfy (== c)
 
 -- Recunoasterea unui cuvant cheie caracter
 string :: String -> Parser String
-string [] = Parser (\s -> [([],s)])
-string (x:xs) = Parser f  
- where
-   f s = [(y:z,zs)| (y,ys)<- apply (char x) s, (z,zs) <- apply (string xs) ys]    
+string [] = Parser (\s -> [([], s)])
+string (x : xs) = Parser f
+  where
+    f s = [(y : z, zs) | (y, ys) <- apply (char x) s, (z, zs) <- apply (string xs) ys]
 
--- Exercitiul 1    
+-- Exercitiul 1
 
 three :: Parser (Char, Char)
-three = undefined
-         
+three = Parser f
+  where
+    f (a : b : c : t) = [((a, c), t)]
+    f _ = []
+
+three2 :: Parser (Char, Char)
+three2 = Parser f
+  where
+    f s = [((a, c), t) | (a, l1) <- apply anychar s, (b, l2) <- apply anychar l1, (c, t) <- apply anychar l2]
+
 -- Exercitiul 2
 
 instance Monad Parser where
-    return x  = Parser (\s ->[(x,s)])
-    m >>= k   = Parser (\s ->[
-      (y,u) | 
-      (x,t) <- apply m s,
-      (y,u) <- apply (k x) t ])
-                     
-                     
+  return x = Parser (\s -> [(x, s)])
+  m >>= k =
+    Parser
+      ( \s ->
+          [ (y, u)
+            | (x, t) <- apply m s,
+              (y, u) <- apply (k x) t
+          ]
+      )
+
 instance Applicative Parser where
   pure = return
   mf <*> ma = do
     f <- mf
     a <- ma
-    return (f a)       
+    return (f a)
 
-instance Functor Parser where              
-   fmap f ma = pure f <*> ma 
-                       
+instance Functor Parser where
+  -- fmap f ma = pure f <*> ma
+  fmap f ma = Parser (\s -> [(f v, t) | (v, t) <- (apply ma s)]) --aplic un parser pe s si il pun un (v,t) iar dupa aplic f pe v
 
-          
 -- Exercitiul 3
 
-anycharord = undefined          
-          
+anycharord :: Parser Int
+anycharord = fmap ord anychar
+
 -- Exercitiul 4
-                     
-failM = Parser (\s ->[])
+
+failM = Parser (\s -> [])
 
 instance MonadPlus Parser where
-    mzero      = failM
-    mplus m n  = Parser (\s -> apply m s ++ apply n s)
+  mzero = failM
+  mplus m n = Parser (\s -> apply m s ++ apply n s)
 
 instance Alternative Parser where
-  empty  = mzero
-  (<|>) = mplus 
-
+  empty = mzero
+  (<|>) = mplus
 
 satisfyM :: (Char -> Bool) -> Parser Char
-satisfyM p = do 
-            c <- anychar
-            if p c then return c else failM   
-            
+satisfyM p = do
+  c <- anychar
+  if p c then return c else failM
+
 digit = satisfyM isDigit
-abcP = satisfyM (`elem` ['A','B','C'])
+
+abcP = satisfyM (`elem` ['A', 'B', 'C'])
 
 alt :: Parser a -> Parser a -> Parser a
 alt p1 p2 = Parser f
-          where f s = apply p1 s ++ apply p2 s   
- 
-manyP :: Parser a -> Parser [ a ]
-manyP p = someP p <|> return [ ]
+  where
+    f s = apply p1 s ++ apply p2 s
 
+manyP :: Parser a -> Parser [a]
+manyP p = someP p <|> return []
 
-someP :: Parser a -> Parser [ a ]
-someP p = do 
-          x <- p
-          xs <- manyP p
-          return ( x : xs )
-
+someP :: Parser a -> Parser [a]
+someP p = do
+  x <- p
+  xs <- manyP p
+  return (x : xs)
 
 identifier :: Parser Char -> Parser Char -> Parser String
-identifier firstCh nextCh = do 
-                             c <- firstCh
-                             s <- many nextCh
-                             return ( c : s )
- 
+identifier firstCh nextCh = do
+  c <- firstCh
+  s <- many nextCh
+  return (c : s)
+
 decimal :: Parser Int
-decimal = do 
-           s <- someP digit
-           return ( read s )
+decimal = do
+  s <- someP digit
+  return (read s)
 
 negative :: Parser Int
-negative = do     char '-'
-                  n <- decimal
-                  return (-n)
-                  
+negative = do
+  char '-'
+  n <- decimal
+  return (- n)
+
 integer :: Parser Int
 integer = decimal `mplus` negative
-  
+
 skipSpace :: Parser ()
-skipSpace = do 
-              _ <- many ( satisfyM isSpace )
-              return ()
-              
+skipSpace = do
+  _ <- many (satisfyM isSpace)
+  return ()
+
 tokenS :: Parser a -> Parser a
-tokenS p = do 
-           skipSpace
-           x <- p
-           skipSpace
-           return x     
+tokenS p = do
+  skipSpace
+  x <- p
+  skipSpace
+  return x
 
 -- Exercitiul 5
 howmany :: Char -> Parser Int
-howmany c = undefined      
+howmany c = fmap length (someP (char c))
 
+-- Exercitiul 6
+twocharord = Parser f
+  where
+    f (a : b : t) = [(ord a + ord b, t)]
+    f_ = []
 
--- Exercitiul 6 
-twocharord = undefined
-                    
-                    
+twocharord2 = fmap (+) anycharord <*> anycharord
+
 -- Exercitiul 7
 no :: Int -> Int -> Int -> Int -> Int
-no x y z v = x*1000+y*100+z*10 + v                  
+no x y z v = x * 1000 + y * 100 + z * 10 + v
 
-fourdigit = undefined
-
- 
+fourdigit = Parser f
+  where
+    f (a : b : c : d : t) = if (all (isDigit) [a, b, c, d]) then [(no (ord a - ord '0') (ord b - ord '0') (ord c - ord '0') (ord d - ord '0'), t)] else []
+    f _ = []
